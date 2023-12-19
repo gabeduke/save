14bd7bf0f403a38ae74f5a9c1d8bfc5052746806
Gabriel Duke <gabeduke@gmail.com>
2023-12-19 09:49:12 -0500

    add aws profile

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/gookit/ini v1.1.1
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/sirupsen/logrus v1.8.1
+	github.com/spf13/pflag v1.0.5
 )
 
 require (
--- main.go
+++ main.go
@@ -1,25 +1,21 @@
 package main
 
 import (
+	"os"
+	"path"
+
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/gookit/ini"
 	"github.com/mitchellh/go-homedir"
 	log "github.com/sirupsen/logrus"
-	"os"
-	"path"
+	flag "github.com/spf13/pflag"
 )
 
 var profiles = []string{"default"}
 
 func main() {
 
-	log.Debug("parsing Keycloak profile from environment")
-	awsrole, found := os.LookupEnv("AWS_KEYCLOAK_PROFILE")
-
-	if found {
-		profiles = append(profiles, awsrole)
-	}
-
+	setup()
 
 	log.Debug("parsing aws credentials from default chain")
 	sess := session.Must(session.NewSession())
@@ -46,9 +42,9 @@ func main() {
 
 	for _, s := range profiles {
 		awsCredentialsFile.SetSection(s, map[string]string{
-			"aws_access_key_id": creds.AccessKeyID,
-			"aws_secret_access_key"	: creds.SecretAccessKey,
-			"aws_session_token"	: creds.SessionToken,
+			"aws_access_key_id":     creds.AccessKeyID,
+			"aws_secret_access_key": creds.SecretAccessKey,
+			"aws_session_token":     creds.SessionToken,
 		})
 	}
 
@@ -57,3 +53,18 @@ func main() {
 	log.Info("Write AWS Session to file successful!")
 }
 
+func setup() {
+	var profile string
+
+	flag.StringVarP(&profile, "profile", "p", "", "AWS profile to use")
+	flag.Parse()
+
+	log.Debug("parsing Keycloak profile from environment")
+	if awsrole, found := os.LookupEnv("AWS_KEYCLOAK_PROFILE"); found {
+		profiles = append(profiles, awsrole)
+	}
+
+	if profile != "" {
+		profiles = append(profiles, profile)
+	}
+}
